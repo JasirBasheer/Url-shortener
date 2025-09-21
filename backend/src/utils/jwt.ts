6ef457db92f84d903c8e2312b9f67ce8fda42d18
JASIR BASHEER <jasirbinbasheerpp@gmail.com>
2025-09-22 02:00:30 +0530

    fix: config import vercel isssue

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import { env } from '@/config/env';
+import { env } from '../config/env';
 import { TokenPair, TokenPayload } from '@/types';
 
 export function generateAccessToken(payload: Omit<TokenPayload, 'type'>): string {
